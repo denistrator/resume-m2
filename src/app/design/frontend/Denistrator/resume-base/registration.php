89fa49bad16ca4b77498bc7215e3f1cdcd4c424b
denistrator <denistrator@yandex.ua>
2019-04-13 23:48:54 +0300

    create "Denistrator Resume Base" theme

---
+++ src/app/design/frontend/Denistrator/resume-base/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © 2019 Denistrator. No rights reserved.
+ * bitch
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Denistrator/resume-base', __DIR__);
