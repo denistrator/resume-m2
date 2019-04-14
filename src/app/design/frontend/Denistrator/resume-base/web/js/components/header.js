ebe1d0b0649cea94ec161fdd5a1ac40a90f31c44
denistrator <denistrator@yandex.ua>
2019-04-14 18:23:47 +0300

    add static / page html

---
+++ src/app/design/frontend/Denistrator/resume-base/web/js/components/header.js
@@ -0,0 +1,37 @@
+(function () {
+    var readingModeToggle = document.querySelector('.reading-mode-toggle');
+
+    readingModeToggle.addEventListener('click', function () {
+        var body = document.querySelector('body.page');
+
+        body.classList.toggle('light');
+
+        if (body.classList.contains('light')) {
+            readingModeToggle.classList.remove('dark');
+            readingModeToggle.classList.add('light');
+        } else {
+            readingModeToggle.classList.remove('light');
+            readingModeToggle.classList.add('dark');
+        }
+    }, false);
+
+    var langToggle = document.querySelector('.lang-toggle');
+    var langToggleText = document.querySelector('.lang-toggle-text');
+    var html = document.querySelector('html');
+
+    langToggle.addEventListener('click', function () {
+        var currentLang = html.getAttribute('lang');
+        var lang, locale;
+        switch (currentLang) {
+            case 'en':
+                lang = 'ru';
+                locale = 'Ru';
+                break;
+            default:
+                lang = 'en';
+                locale = 'En';
+        }
+        html.setAttribute('lang', lang);
+        langToggleText.innerHTML = locale;
+    }, false);
+})();
