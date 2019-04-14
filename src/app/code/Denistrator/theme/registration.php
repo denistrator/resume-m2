690a1d434ce1cdb17a82835bac3fe0daca5fb714
denistrator <denistrator@yandex.ua>
2019-04-14 19:02:11 +0300

    create "Denistrator_Theme" module

---
+++ src/app/code/Denistrator/theme/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © 2019 Denistrator. No rights reserved.
+ * bitch
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Denistrator_Theme', __DIR__);
